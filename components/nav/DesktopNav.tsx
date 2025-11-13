30abee940e8f35d67d799a8ebbb5f4059115da4f
Rashad Tyler <rashadtyler@Rashads-Mac-Studio.local>
2025-11-13 17:36:43 -0600

    style: extend desktop journal cta height

--- components/nav/DesktopNav.tsx
+++ components/nav/DesktopNav.tsx
@@ -31,10 +31,10 @@ export const DesktopNav = ({
         ))}
       </div>
       <div className="flex items-center gap-4 text-base font-semibold text-white">
-        <div className="flex h-full">
+        <div className="flex">
           <a
             href={navigation.topLinks.journal.href}
-            className="flex h-full items-center gap-2 rounded-full bg-gradient-to-r from-indigo-500/40 via-purple-500/40 to-pink-500/40 px-6 transition hover:from-indigo-500/60 hover:via-purple-500/60 hover:to-pink-500/60"
+            className="-my-4 flex items-center gap-2 rounded-full bg-gradient-to-r from-indigo-500/40 via-purple-500/40 to-pink-500/40 px-6 py-4 transition hover:from-indigo-500/60 hover:via-purple-500/60 hover:to-pink-500/60"
             target="_blank"
             rel="noopener noreferrer"
           >
