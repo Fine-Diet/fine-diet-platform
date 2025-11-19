9983e8f7b133b9b61abc6f8425878dce4e9cafd1
Rashad Tyler <rashadtyler@me.com>
2025-11-18 21:20:36 -0600

    Update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           </div>
         ))}
         {homeContent.gridSections?.map((section, index) => (
-          <div key={`grid-${index}`} className="px-3 pb-3 pt-1.5">
+          <div key={`grid-${index}`} className="px-3 pb-3 pt-3">
             <GridSection section={section} />
           </div>
         ))}
