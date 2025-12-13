9713437df450a2c8562361b4e7caec8317329700
Rashad Tyler <22127403+rashadtyler@users.noreply.github.com>
2025-12-12 18:08:23 -0600

    Add dotted border style to NavDrawerLeft

--- components/nav/NavDrawerLeft.tsx
+++ components/nav/NavDrawerLeft.tsx
@@ -16,7 +16,7 @@ export const NavDrawerLeft = ({
   onItemSelect,
 }: NavDrawerLeftProps) => {
   return (
-    <div className="w-full md:w-1/5 md:min-w-[230px] border-r border-r-[.5px] border-white/5 pl-10 pr-2 pt-[47px] space-y-10">
+    <div className="w-full md:w-1/5 md:min-w-[230px] border-dotted border-r border-r-[.5px] border-white/5 pl-10 pr-2 pt-[47px] space-y-10">
       {subcategories.map((subcategory) => {
         return (
           <div key={subcategory.id} className="space-y-2">
